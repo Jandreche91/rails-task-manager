36290a35feea27d49240fdb4d38a95e07700825b
javier echeverri <jecheverrid@gmail.com>
2020-05-09 15:14:51 +0200

    update task is implemented

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,26 +1,3 @@
 class ApplicationController < ActionController::Base
-  def index
-    @all_tasks = Task.all
-  end
 
-  def show
-    @current_task = Task.find(params[:id])
-    @message = @current_task.completed ? "Task completed" : "You haven't completed this task"
-  end
-
-  def new
-    @new_task = Task.new
-  end
-
-  def create
-    @new_task = Task.new(task_params)
-    @new_task.save
-    redirect_to task_path(@new_task)
-  end
-
-  private
-
-  def task_params
-    params.require(:task).permit(:title, :details)
-  end
 end
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,38 @@
 class TasksController < ApplicationController
+
+  def index
+    @all_tasks = Task.all
+  end
+
+  def new
+    @new_task = Task.new
+  end
+
+  def show
+    @current_task = Task.find(params[:id])
+    @message = @current_task.completed ? "Task completed" : "You haven't completed this task"
+  end
+
+  def create
+    @new_task = Task.new(task_params)
+    @new_task.save
+    redirect_to task_path(@new_task)
+  end
+
+  def edit
+    @current_task = Task.find(params[:id])
+  end
+
+  def update
+    @current_task = Task.find(params[:id])
+    @current_task.update(task_params)
+    # raise
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
 end
