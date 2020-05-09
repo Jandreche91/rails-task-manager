ccb6ee62df10efdf9a631e9d1bd4d3d02d78890b
javier echeverri <jecheverrid@gmail.com>
2020-05-09 10:59:37 +0200

    read tasks implemented

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  def index
+    @all_tasks = Task.all
+  end
+
+  def show
+    @current_task = Task.find(params[:id])
+    @message = @current_task.completed ? "Task completed" : "You haven't completed this task"
+  end
+
+  def new
+
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+10.times do
+  a_task = Task.new()
+  a_task.title = Faker::House.room
+  a_task.details = Faker::Lorem.sentence
+  a_task.save
+end
